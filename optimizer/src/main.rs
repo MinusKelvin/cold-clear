eb32c2eb44434e1425d43007c642c34ad796cbf2
MinusKelvin <mark.carlson@minuskelvin.net>
2019-11-18 23:55:32 +1100

    probably fix weird memory issue

--- optimizer/src/main.rs
+++ optimizer/src/main.rs
@@ -2,13 +2,16 @@ use serde::{ Serialize, Deserialize };
 use bot::evaluation::Standard;
 use rand::prelude::*;
 use libflate::deflate;
+use std::sync::{ Arc, Mutex };
+use std::collections::VecDeque;
+use std::sync::mpsc::channel;
 
 mod battle;
 mod mutate;
 
 use mutate::Mutateable;
 
-const BATTLES: usize = 4;
+const BATTLES: usize = 10;
 
 fn main() {
     let mut population = match std::fs::File::open("pop.json") {
@@ -19,47 +22,44 @@ fn main() {
         Err(_) => new_population::<Standard>()
     };
 
-    let (replay_saver, recv) = crossbeam_channel::unbounded();
-    let replay_save_thread = std::thread::spawn(move || {
-        while let Ok(replay) = recv.recv() {
-            let mut encoder = deflate::Encoder::new(
-                std::fs::File::create("recent-game.dat"
-            ).unwrap());
-            bincode::serialize_into(&mut encoder, &replay).unwrap();
-            encoder.finish().unwrap();
-        }
-    });
-
-    let (matchups, recv) = crossbeam_channel::unbounded();
-    let (send, game_results) = crossbeam_channel::unbounded();
+    let matchups = Arc::new(Mutex::new((true, VecDeque::new())));
+    let (send, game_results) = channel();
     for _ in 0..12 {
-        let recv = recv.clone();
+        let matchups = matchups.clone();
         let send = send.clone();
-        let replay_saver = replay_saver.clone();
         std::thread::spawn(move || {
-            while let Ok((p1, p1_e, p2, p2_e)) = recv.recv() {
+            loop {
+                let (p1, p1_e, p2, p2_e) = {
+                    let (active, ref mut queue) = *matchups.lock().unwrap();
+                    if !active { break }
+                    match queue.pop_front() {
+                        Some(v) => v,
+                        None => continue
+                    }
+                };
                 if let Some((replay, p1_won)) = battle::do_battle(p1_e, p2_e) {
-                    replay_saver.send(replay).ok();
-                    send.send(Some(if p1_won { p1 } else { p2 })).ok();
+                    send.send(Some((if p1_won { p1 } else { p2 }, replay))).ok();
                 } else {
                     send.send(None).ok();
                 }
             }
         });
     }
-    drop(replay_saver);
 
     loop {
         let mut count = 0;
-        for i in 0..population.members.len() {
-            for j in 0..population.members.len() {
-                if i == j { continue }
-                for _ in 0..BATTLES {
-                    matchups.send((
-                        i, population.members[i].clone(),
-                        j, population.members[j].clone()
-                    )).ok();
-                    count += 1;
+        {
+            let mut matchups = matchups.lock().unwrap();
+            for i in 0..population.members.len() {
+                for j in 0..population.members.len() {
+                    if i == j { continue }
+                    for _ in 0..BATTLES {
+                        matchups.1.push_back((
+                            i, population.members[i].clone(),
+                            j, population.members[j].clone()
+                        ));
+                        count += 1;
+                    }
                 }
             }
         }
@@ -69,10 +69,16 @@ fn main() {
             results.push((i, 0));
         }
         for i in 0..count {
-            if let Some(winner) = game_results.recv().unwrap() {
+            if let Some((winner, replay)) = game_results.recv().unwrap() {
                 results[winner].1 += 1;
+
+                let mut encoder = deflate::Encoder::new(
+                    std::fs::File::create("recent-game.dat").unwrap()
+                );
+                bincode::serialize_into(&mut encoder, &replay).unwrap();
+                encoder.finish().unwrap();
             }
-            if (i+1) % 10 == 0 {
+            if (i+1) % 100 == 0 {
                 println!("Completed game {} of {}", i+1, count);
             }
         }
@@ -124,8 +130,7 @@ fn main() {
         population = new_population;
     }
 
-    drop(matchups);
-    replay_save_thread.join().ok();
+    matchups.lock().unwrap().0 = false;
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
