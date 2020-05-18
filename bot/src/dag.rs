39f558bd08935f669cf2fe397dcf3e2a881fb087
MinusKelvin <mark.carlson@minuskelvin.net>
2020-05-18 23:54:57 +1000

    add unmark, update_known, update_speculated

--- bot/src/dag.rs
+++ bot/src/dag.rs
@@ -34,9 +34,38 @@
 //! by only de/allocating in large chunks, using `bumpalo` for the children lists is the main
 //! optimization there. `bumpalo` has an interesting strategy for allocating new chunks of memory,
 //! if need be we can replace the `Vec` we're using as a slab with something with a similar
-//! allocation strategy. If memory usage is a problem we can compact `SimplifiedBoard`'s grid using
-//! the `bitvec` crate. If allocator performance is still an issue, we could pool the bump
+//! allocation strategy. If allocator performance is still an issue, we could pool the bump
 //! allocation arenas and the slab `Vec`s, but this seems unlikely.
+//! 
+//! ## Some overall memory usage stuff
+//! 
+//! The hashmaps of `SimplifiedBoard`s will probably take up quite a bit of space, since a tetris
+//! board is a large thing (400 cells!). We currently use the simple bitboard slice strategy. If
+//! need be, we can easily switch to the compact bitboard slice strategy by using the `bitvec`
+//! crate. I don't think my run-length encoding scheme saves enough memory to be worth the effort,
+//! but I think it's cool. Quick overview of the memory usage of various strategies:
+//! - Naive `[[bool; 10]; 40]`: 400 bytes
+//! - Simple bitboard `[u16; 40]`: 80 bytes
+//! - Compact bitboard `[u1; 400]`: 50 bytes
+//! - Simple bitboard slice `&[u16]` (omit empty rows): 36 bytes-ish
+//! - Compact bitboard slice `&[u1]` (omit trailing empty cells): 28 bytes-ish
+//! - Run-length encoding scheme (using `&[u8]`): 32 bytes-ish
+//! - Run-length encoding scheme (using `*const u8`): 25 bytes-ish
+//! The "-ish" values are based on this board, which I assume is typical: http://fumen.zui.jp/?v115@BgA8CeA8EeA8BeD8CeF8CeH8AeK8AeI8AeI8AeE8Ae?I8AeI8AeD8JeAgH
+//! 
+//! My run-length encoding scheme is not self-explanatory, so I will describe it here (despite the
+//! lack of any implementation). Represent the cells as a bitstring ordered as column 0 going up,
+//! column 1 going down, column 2 going up, column 3 going down, etc. Represent each run as a byte
+//! where the topmost bit is 1 if the run is of filled cells and 0 if it is of empty cells. The
+//! remaining 7 bits store the length of the run minus 1. If a run is longer than 128, it is
+//! transformed to a run of length 128 plus a run of the remaining length. Since the length of the
+//! byte sequence is represented in the data, the pointer can be made thin, but this requires the
+//! use of `unsafe` code when decoding.
+//! 
+//! Example 1: the empty field is represented as `0x7F 0x7F 0x7F 0x0F`; three runs of empty cells of
+//! length 128, and a run of empty cells of length 16. Example 2: the field containing only an I
+//! piece laid flat in the center is represented as `0x7F 0x1E 0x81 0x4D 0x81 0x7F 0x1E`.
+#![allow(dead_code)]
 
 use libtetris::{ Board, Piece, FallingPiece };
 use std::collections::{ HashMap, VecDeque };
@@ -44,6 +73,7 @@ use smallvec::SmallVec;
 use arrayvec::ArrayVec;
 use enumset::EnumSet;
 use enum_map::EnumMap;
+use serde::{ Serialize, Deserialize };
 use rand::prelude::*;
 use crate::evaluation::Evaluation;
 
@@ -52,6 +82,20 @@ pub struct DagState<E: 'static, R: 'static> {
     generations: VecDeque<rented::Generation<E, R>>,
     root: u32,
     pieces: u32
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct NodeId {
+    generation: u32,
+    slab_key: u32
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChildData<E, R> {
+    pub mv: FallingPiece,
+    pub board: Board,
+    pub evaluation: E,
+    pub reward: R,
 }
 
 rental! {
@@ -71,7 +115,9 @@ struct Generation<'c, E, R> {
 }
 
 enum Children<'c, R> {
-    Known(Vec<Option<&'c mut [Child<R>]>>),
+    // we need to know the piece to resolve speculations computed before a new piece was added,
+    // but given to us after a new piece was added.
+    Known(Piece, Vec<Option<&'c mut [Child<R>]>>),
     Speculated(Vec<Option<EnumMap<Piece, Option<&'c mut [Child<R>]>>>>)
 }
 
@@ -108,7 +154,8 @@ impl<E: Evaluation<R> + 'static, R: Clone + 'static> DagState<E, R> {
             next_pieces.next();
         }
         for piece in next_pieces {
-            generations.push_back(rented::Generation::known());
+            // technically the piece given here is never used, but might as well put it here
+            generations.push_back(rented::Generation::known(piece));
         }
         let root_generation = rented::Generation::new(
             Box::new(bumpalo::Bump::new()),
@@ -119,7 +166,7 @@ impl<E: Evaluation<R> + 'static, R: Clone + 'static> DagState<E, R> {
                     marked: false,
                     death: false
                 }],
-                children: Children::Known(vec![None]),
+                children: Children::Known(Piece::I, vec![None]), // nonsense piece never used
                 deduplicator: HashMap::new() // nothing else will ever be put in this generation
             }
         );
@@ -138,13 +185,8 @@ impl<E: Evaluation<R> + 'static, R: Clone + 'static> DagState<E, R> {
         for i in (0..forced_analysis_lines.len()).rev() {
             let mut path = &*forced_analysis_lines[i];
             let mut done = false;
-            let choice = self.find_and_mark_leaf_with_chooser(|_, children| match path {
-                &[] => {
-                    // already analysed this path, so we're done with it
-                    done = true;
-                    None
-                }
-                &[next, ref rest@..] => {
+            let choice = self.find_and_mark_leaf_with_chooser(|_, children| {
+                if let &[next, ref rest @ ..] = path {
                     let mut target = next.cells();
                     target.sort();
                     for child in children {
@@ -160,10 +202,10 @@ impl<E: Evaluation<R> + 'static, R: Clone + 'static> DagState<E, R> {
                             return Some(child)
                         }
                     }
-                    // can't find the next step on the path, so we're done with it
-                    done = true;
-                    None
                 }
+                // can't find the next step on the path, so we're done with it
+                done = true;
+                None
             });
             if done {
                 forced_analysis_lines.swap_remove(i);
@@ -199,9 +241,8 @@ impl<E: Evaluation<R> + 'static, R: Clone + 'static> DagState<E, R> {
         let mut gen_index = 0;
         let mut node_key = self.root as usize;
         loop {
-            let node = self.generations[gen_index].ref_rent(|gen| &gen.nodes[node_key]);
             let children = self.generations[gen_index].maybe_ref_rent(|gen| match &gen.children {
-                Children::Known(childrens) => childrens[node_key].as_deref(),
+                Children::Known(_, childrens) => childrens[node_key].as_deref(),
                 Children::Speculated(childrens) => {
                     // We must select a single group of children to search further. We do this by
                     // randomly selecting a valid next piece and using the child group associated
@@ -219,32 +260,114 @@ impl<E: Evaluation<R> + 'static, R: Clone + 'static> DagState<E, R> {
                 }
             });
 
-            let children = match children {
-                // branch
-                Some(v) => v,
-                // leaf
-                None => if node.marked {
-                    // this node has already been returned for processing, so we failed to find
-                    // a leaf.
-                    return None
-                } else {
-                    // found a valid leaf, so mark it and return
-                    self.generations[gen_index].rent_mut(|gen| gen.nodes[node_key].marked = true);
-                    return Some((NodeId {
-                        generation: gen_index as u32 + self.pieces,
-                        slab_key: node_key as u32
-                    }, board))
-                }
-            };
-
-            let child = chooser(
-                self.generations[gen_index+1].ref_rent(|gen| &gen.nodes),
-                children
-            )?;
-            advance(&mut board, child.placement);
-            gen_index += 1;
-            node_key = child.node as usize;
+            if let Some(children) = children {
+                let child = chooser(
+                    self.generations[gen_index+1].ref_rent(|gen| &gen.nodes),
+                    children
+                )?;
+                advance(&mut board, child.placement);
+                gen_index += 1;
+                node_key = child.node as usize;
+            } else if self.generations[gen_index].rent(|gen| gen.nodes[node_key].marked) {
+                // this leaf has already been returned for processing, so it's not valid
+                return None
+            } else {
+                // found a valid leaf, so mark it and return
+                self.generations[gen_index].rent_mut(|gen| gen.nodes[node_key].marked = true);
+                return Some((NodeId {
+                    generation: gen_index as u32 + self.pieces,
+                    slab_key: node_key as u32
+                }, board))
+            }
         }
+    }
+
+    pub fn update_known(&mut self, node: NodeId, children: Vec<ChildData<E, R>>) {
+        // make sure we weren't given a NodeId for an expired node. it could happen.
+        if node.generation < self.pieces {
+            return
+        }
+        let gen = (node.generation - self.pieces) as usize;
+
+        let [parent_gen, child_gen] = self.get_gen_and_next(gen);
+
+        parent_gen.rent_all_mut(|current| child_gen.rent_all_mut(|mut next|
+            match &mut current.data.children {
+                Children::Known(_, c) => c[node.slab_key as usize] = Some(build_children(
+                    current.arena, &mut next, children, node.slab_key
+                )),
+                Children::Speculated(_) => unreachable!()
+            }
+        ));
+    }
+
+    pub fn update_speculated(
+        &mut self, node: NodeId,
+        mut children: EnumMap<Piece, Option<Vec<ChildData<E, R>>>>
+    ) {
+        // make sure we weren't given a NodeId for an expired node. it could happen.
+        if node.generation < self.pieces {
+            return
+        }
+        let gen = (node.generation - self.pieces) as usize;
+
+        let [parent_gen, child_gen] = self.get_gen_and_next(gen);
+
+        parent_gen.rent_all_mut(|current| child_gen.rent_all_mut(|mut next|
+            match &mut current.data.children {
+                Children::Known(piece, c) => c[node.slab_key as usize] = Some(build_children(
+                    current.arena, &mut next, children[*piece].take().unwrap(), node.slab_key
+                )),
+                Children::Speculated(c) => {
+                    let mut childs = EnumMap::new();
+                    for (p, data) in children {
+                        if let Some(data) = data {
+                            childs[p] = Some(build_children(
+                                current.arena, &mut next, data, node.slab_key
+                            ));
+                        }
+                    }
+                    c[node.slab_key as usize] = Some(childs);
+                }
+            }
+        ));
+    }
+
+    fn get_gen_and_next(&mut self, gen: usize) -> [&mut rented::Generation<E, R>; 2] {
+        if gen == self.generations.len() - 1 {
+            // we're expanding into boards that belong in a generation that doesn't exist yet.
+            // since it doesn't exist, we're missing some next queue information, so it's a
+            // speculated generation.
+            self.generations.push_back(rented::Generation::speculated());
+        }
+
+        // this is legit the easiest way to get mutable references to the relevant generations
+        let mut iter = self.generations.iter_mut().skip(gen);
+        let current_gen = iter.next().unwrap();
+        let next_gen = iter.next().unwrap();
+
+        [current_gen, next_gen]
+    }
+
+    pub fn unmark(&mut self, node: NodeId) {
+        // make sure we weren't given a NodeId for an expired node. it could happen.
+        if node.generation >= self.pieces {
+            self.generations[(node.generation - self.pieces) as usize].rent_mut(
+                |gen| gen.nodes[node.slab_key as usize].marked = false
+            );
+        }
+    }
+
+    pub fn nodes(&self) -> u32 {
+        0
+    }
+
+    pub fn is_dead(&self) -> bool {
+        self.generations[0].rent(|gen| match &gen.children {
+            Children::Known(_, childrens) =>
+                childrens[self.root as usize].as_ref().unwrap().is_empty(),
+            Children::Speculated(_) => unreachable!()
+        })
     }
 }
 
@@ -259,14 +382,68 @@ fn advance(board: &mut Board, placement: FallingPiece) {
     }
 }
 
+fn build_children<'arena, E: Evaluation<R> + 'static, R: Clone + 'static>(
+    parent_arena: &'arena bumpalo::Bump,
+    children_gen: &mut rented::Generation_BorrowMut<E, R>,
+    mut children: Vec<ChildData<E, R>>,
+    parent: u32
+) -> &'arena mut [Child<R>] {
+    // best to worst
+    children.sort_by_key(
+        |c| std::cmp::Reverse(c.evaluation.clone() + c.reward.clone())
+    );
+    parent_arena.alloc_slice_fill_iter(
+        children.into_iter().enumerate().map(|(i, data)| {
+            // maybe i should expose the backing array in libtetris::Board; it would avoid a copy
+            let mut simple_grid = ArrayVec::<[_; 40]>::new();
+            for y in 0..*data.board.column_heights().iter().max().unwrap() {
+                simple_grid.push(*data.board.get_row(y));
+            }
+            let simple_board = SimplifiedBoard {
+                grid: children_gen.arena.alloc_slice_copy(&simple_grid),
+                back_to_back: data.board.b2b_bonus,
+                combo: data.board.combo,
+                bag: data.board.next_bag(),
+                reserve: data.board.hold_piece.unwrap_or_else(
+                    || data.board.next_queue().next().unwrap()
+                ),
+                reserve_is_hold: data.board.hold_piece.is_some()
+            };
+
+            let nodes = &mut children_gen.data.nodes;
+            let evaluation = data.evaluation;
+            let &mut node = children_gen.data.deduplicator
+                .entry(simple_board)
+                .or_insert_with(|| {
+                    let node = nodes.len();
+                    nodes.push(Node {
+                        parents: SmallVec::new(),
+                        evaluation,
+                        death: false,
+                        marked: false
+                    });
+                    node as u32
+                });
+            children_gen.data.nodes[node as usize].parents.push(parent);
+
+            Child {
+                placement: data.mv,
+                original_rank: i as u32,
+                reward: data.reward,
+                node
+            }
+        })
+    )
+}
+
 impl<E: 'static, R: 'static> rented::Generation<E, R> {
-    pub fn known() -> Self {
+    pub fn known(piece: Piece) -> Self {
         rented::Generation::new(
             Box::new(bumpalo::Bump::new()),
             |_| Generation {
                 nodes: vec![],
                 deduplicator: HashMap::new(),
-                children: Children::Known(vec![])
+                children: Children::Known(piece, vec![])
             }
         )
     }
@@ -281,9 +458,4 @@ impl<E: 'static, R: 'static> rented::Generation<E, R> {
             }
         )
     }
-}
-
-pub struct NodeId {
-    generation: u32,
-    slab_key: u32
 }
